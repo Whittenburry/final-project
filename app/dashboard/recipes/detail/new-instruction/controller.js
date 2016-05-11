8f0e3cfde1d10f7367b5fb8109d3d4ea1ba1424d
Emily Znamierowski <emilyannz@gmail.com>
2016-05-11 18:43:15 -0500

    Updated new instruction controller-now working!

--- app/dashboard/recipes/detail/new-instruction/controller.js
+++ app/dashboard/recipes/detail/new-instruction/controller.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  addInstruction(formValues) {
+  addInstruction(parentRecipe, formValues) {
     const instruction = this.store.createRecord(`instruction`, formValues);
+    instruction.set(`recipe`, parentRecipe);
+    // instruction.recipe = parentRecipe;
 
     instruction.save().then(() => {
-      this.transitionToRoute(`dashboard.recipes.detail.index`)
+      this.transitionToRoute(`dashboard.recipes.detail.index`);
     });
   },
 });
