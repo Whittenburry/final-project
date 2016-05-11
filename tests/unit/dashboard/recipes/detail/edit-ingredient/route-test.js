925d10f7d61632503012ead1326a168f4a979fb5
Emily Znamierowski <emilyannz@gmail.com>
2016-05-11 13:54:40 -0500

    Created detail routes for ingredient and instruction

---
+++ tests/unit/dashboard/recipes/detail/edit-ingredient/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:dashboard/recipes/detail/edit-ingredient', 'Unit | Route | dashboard/recipes/detail/edit ingredient', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
---
+++ tests/unit/dashboard/recipes/detail/new-ingredient/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:dashboard/recipes/detail/new-ingredient', 'Unit | Route | dashboard/recipes/detail/new ingredient', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
