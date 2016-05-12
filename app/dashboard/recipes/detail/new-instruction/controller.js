44ed029c4685cd028d7c4ab146e361d6c23aabad
Emily Znamierowski <emilyannz@gmail.com>
2016-05-11 19:02:13 -0500

    Edited new-instruction controller and template to auto count

--- app/dashboard/recipes/detail/new-instruction/controller.js
+++ app/dashboard/recipes/detail/new-instruction/controller.js
@@ -2,12 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   addInstruction(parentRecipe, formValues) {
-    const instruction = this.store.createRecord(`instruction`, formValues);
-    instruction.set(`recipe`, parentRecipe);
-    // instruction.recipe = parentRecipe;
+    // Wait to load all the instructions for the current recipe
+    parentRecipe.get(`instructions`).then(() => {
+      // How many steps exist already? Let's add one to that
+      const step = parentRecipe.get(`instructions.length`) + 1;
 
-    instruction.save().then(() => {
+      // Setup our base instruction
+      const instruction = this.store.createRecord(`instruction`, formValues);
+
+      // Setup the relation to our recipe
+      instruction.set(`recipe`, parentRecipe);
+      // instruction.recipe = parentRecipe;
+      // Save our calculated step number
+      instruction.set(`step`, step);
+
+      // Wait for save
+      return instruction.save();
+    }).then(() => {
       this.transitionToRoute(`dashboard.recipes.detail.index`);
     });
   },
 });
+
+// The current indegrident doesn't need to know about the last ingredient, just the recipe; no calculations
+// needed for edit forms
