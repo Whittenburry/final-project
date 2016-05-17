df91c1f89327b934eda6cc62f3883bb77088733d
John Whittenburg <jawhittenburg@gmail.com>
2016-05-17 14:53:55 -0500

    a user can respond to one recipe at a time

--- app/register/route.js
+++ app/register/route.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend({
+// Only allow guest users in...
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   model() {
     return {
       email: ``,
