b8dfcbde71542438e86120b0b75a594d8f35fd9b
Emily Znamierowski <emilyannz@gmail.com>
2016-05-17 21:57:23 -0500

    Commented out user profile

--- app/dashboard/user-profile/route.js
+++ app/dashboard/user-profile/route.js
@@ -1,10 +1,10 @@
-import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-
-
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  session: Ember.inject.service(),
-  model() {
-  return this.get('session.data.authenticated.user');
-  },
-});
+// import Ember from 'ember';
+// import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+//
+//
+// export default Ember.Route.extend(AuthenticatedRouteMixin, {
+//   session: Ember.inject.service(),
+//   model() {
+//   return this.get('session.data.authenticated.user');
+//   },
+// });
