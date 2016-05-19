2822c0da7a7a6f318a642883149decc1479f3789
John Whittenburg <jawhittenburg@gmail.com>
2016-05-19 00:25:56 -0500

    user can reset their taste profile

--- app/dashboard/my-recipes/controller.js
+++ app/dashboard/my-recipes/controller.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  resetLikes(user) {
+    if (confirm(`You trynna reset? or naw?`)) {
+      user.get(`responses`).forEach((response) => {
+        response.destroyRecord();
+      });
+    }
+  },
 });
