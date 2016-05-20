23b729b878619144c22091c9be90d5866098309b
John Whittenburg <jawhittenburg@gmail.com>
2016-05-19 19:33:45 -0500

    a user can swipe the hamburger menu away

--- app/helpers/exists-in-both.js
+++ app/helpers/exists-in-both.js
@@ -4,7 +4,6 @@ import _ from 'lodash';
 export function existsInBoth([user]/*, hash*/) {
   return function(responseIds) {
     const userResponses = user.get(`responseIds`);
-    console.log(user.get(`responseIds`));
 
     return _.intersection(responseIds, userResponses).length >= 1;
   };
